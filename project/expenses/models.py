87b1cb175101d608ea49cea9323edaa34b5e8d5e
unknown <Mech-LabVIEW@Mech-LabVIEW.atec.net>
2026-01-13 11:50:25 +0530

    Added category and expenses models with admin registration done

---
+++ project/expenses/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+    
+class Expenses(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, unique=True)
+    amount = models.DecimalField(max_length=10, decimal_places=2)
+    date = models.DateField()
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
