61885d66844539eaad350df6d167fc899b6e4496
unknown <Mech-LabVIEW@Mech-LabVIEW.atec.net>
2026-01-14 11:05:56 +0530

    Add expense list views and templates (expenses_home and category_page)

--- project/expenses/views.py
+++ project/expenses/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category,Expenses
 # Create your views here.
 
 def category_home(request):
     catogories = Category.objects.all()
     context = {'categories': catogories}
     return render(request, 'expenses/category_home.html', context)
+
+def category_page(request):
+    categories = Category.objects.all()     
+    context = {'categories': categories}
+    return render(request, 'expenses/category_page.html', context)
+
+def expenses_home(request):
+    expenses = Expenses.objects.all()
+    context = {'expenses': expenses}    
+    return render(request, 'expenses/expenses_home.html', context)
