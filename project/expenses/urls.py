77b61b6da71b3cadacc63765b1fd7f5e15b0439d
unknown <Mech-LabVIEW@Mech-LabVIEW.atec.net>
2026-01-14 12:24:52 +0530

    Add user signup and login with forms and templates

--- project/expenses/urls.py
+++ project/expenses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import category_home, expenses_home, category_page
+from .views import category_home, expenses_home, category_page, userSignup, userLogin
 urlpatterns = [
      path('', category_home, name='category-home'),
      path('expenses', expenses_home , name='expenses_home'),
      path('category', category_page , name='category_page'),
+     path('user-signup', userSignup, name='user-signup'),
+     path('user-login', userLogin, name='user-login'),
  ]
--- project/expenses/views.py
+++ project/expenses/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category,Expenses
+from .forms import UserForm,UserLoginForm
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 # Create your views here.
 
 def category_home(request):
@@ -16,3 +20,37 @@ def expenses_home(request):
     expenses = Expenses.objects.all()
     context = {'expenses': expenses}    
     return render(request, 'expenses/expenses_home.html', context)
+
+def userSignup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("User signed up successfully!")
+        else:
+            return redirect('user-signup')
+    else:      
+        form = UserForm()
+        context = {'form': form}
+    return render(request, 'expenses/user_signup.html', context)
+
+def userLogin(request):
+    if request.method =="POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            # Authentication logic would go here
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('category-home')
+            else:
+                messages.error(request, "Invalid username or password.")
+                return redirect('user-login')   
+        else:
+            return redirect('user-login')
+    else:
+        form = UserLoginForm()
+        context = {'form': form}    
+    return render(request, 'expenses/user_login.html', context)
