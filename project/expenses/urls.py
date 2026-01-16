4f63b6c653772cda6bb0f75e533bf0e8ade78a18
unknown <Mech-LabVIEW@Mech-LabVIEW.atec.net>
2026-01-16 10:53:43 +0530

    Fix login logic, add user login template, and add gitignore

--- project/expenses/urls.py
+++ project/expenses/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import category_home 
+from .views import category_home,user_signup, user_login
 urlpatterns = [
      path('', category_home, name='category-home'),
+     path('signup/', user_signup, name='user-signup'),
+     path('login/', user_login, name='user-login'),
  ]
